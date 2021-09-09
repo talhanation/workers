7bb35c38029c18c7c9c08acab6034562e6053875
talhanation <talhakantar@yahoo.com>
2021-09-09 21:22:23 +0200

    Update MinerEntity.java

--- src/main/java/com/talhanation/workers/entities/MinerEntity.java
+++ src/main/java/com/talhanation/workers/entities/MinerEntity.java
@@ -1,6 +1,8 @@
 package com.talhanation.workers.entities;
 
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ILivingEntityData;
@@ -11,15 +13,33 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.pathfinding.GroundPathNavigator;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.event.ForgeEventFactory;
 
 import javax.annotation.Nullable;
+import java.util.Optional;
 
 public class MinerEntity extends AbstractWorkerEntity {
+
+    private static final DataParameter<Integer> breakingTime = EntityDataManager.defineId(MinerEntity.class, DataSerializers.INT);
+    private static final DataParameter<Integer> currentTimeBreak = EntityDataManager.defineId(MinerEntity.class, DataSerializers.INT);
+    private static final DataParameter<Integer> previousTimeBreak = EntityDataManager.defineId(MinerEntity.class, DataSerializers.INT);
+
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(breakingTime, 0);
+        this.entityData.define(currentTimeBreak, -1);
+        this.entityData.define(previousTimeBreak, -1);
+    }
 
     public MinerEntity(EntityType<? extends AbstractWorkerEntity> entityType, World world) {
         super(entityType, world);
@@ -31,7 +51,6 @@ public class MinerEntity extends AbstractWorkerEntity {
         return createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 20.0D)
                 .add(Attributes.MOVEMENT_SPEED, 0.3D)
-                .add(Attributes.KNOCKBACK_RESISTANCE, 0.1D)
                 .add(Attributes.ATTACK_DAMAGE, 1.0D)
                 .add(Attributes.FOLLOW_RANGE, 32.0D);
 
@@ -49,17 +68,9 @@ public class MinerEntity extends AbstractWorkerEntity {
     }
     @Override
     public void setEquipment() {
-        this.setItemSlot(EquipmentSlotType.HEAD, new ItemStack(Items.LEATHER_HELMET));
-        this.setItemSlot(EquipmentSlotType.CHEST, new ItemStack(Items.LEATHER_CHESTPLATE));
-        this.setItemSlot(EquipmentSlotType.LEGS, new ItemStack(Items.LEATHER_LEGGINGS));
-        this.setItemSlot(EquipmentSlotType.FEET, new ItemStack(Items.LEATHER_BOOTS));
-        int i = this.random.nextInt(8);
+        int i = this.random.nextInt(9);
         if (i == 0) {
             this.setItemSlot(EquipmentSlotType.MAINHAND, new ItemStack(Items.STONE_PICKAXE));
-        } else  if (i == 1){
-            this.setItemSlot(EquipmentSlotType.MAINHAND, new ItemStack(Items.STONE_SWORD));
-        } else  if (i == 2){
-            this.setItemSlot(EquipmentSlotType.MAINHAND, new ItemStack(Items.STONE_SWORD));
         }else{
             this.setItemSlot(EquipmentSlotType.MAINHAND, new ItemStack(Items.WOODEN_PICKAXE));
         }
@@ -67,7 +78,7 @@ public class MinerEntity extends AbstractWorkerEntity {
 
     @Nullable
     @Override
-    public AgeableEntity getBreedOffspring(ServerWorld p_241840_1_, AgeableEntity p_241840_2_) {
+    public AgeableEntity getBreedOffspring(ServerWorld world, AgeableEntity ageable) {
         return null;
     }
 
@@ -81,5 +92,84 @@ public class MinerEntity extends AbstractWorkerEntity {
         return "Miner";
     }
 
+    public int getBreakingTime() {
+        return this.entityData.get(breakingTime);
+    }
+
+    public void setBreakingTime(int value) {
+        this.entityData.set(breakingTime, value);
+    }
+
+    public int getCurrentTimeBreak() {
+        return this.entityData.get(currentTimeBreak);
+    }
+
+    public void setCurrentTimeBreak(int value) {
+        this.entityData.set(currentTimeBreak, value);
+    }
+
+    public int getPreviousTimeBreak() {
+        return this.entityData.get(previousTimeBreak);
+    }
+
+    public void setPreviousTimeBreak(int value) {
+        this.entityData.set(previousTimeBreak, value);
+    }
+
+    public void addAdditionalSaveData(CompoundNBT compound) {
+        super.addAdditionalSaveData(compound);
+        compound.putInt("breakTime", this.getBreakingTime());
+        compound.putInt("currentTimeBreak", this.getCurrentTimeBreak());
+        compound.putInt("previousTimeBreak", this.getPreviousTimeBreak());
+    }
+
+    public void readAdditionalSaveData(CompoundNBT compound) {
+        super.readAdditionalSaveData(compound);
+        this.setBreakingTime(compound.getInt("breakTime"));
+        this.setCurrentTimeBreak(compound.getInt("currentTimeBreak"));
+        this.setPreviousTimeBreak(compound.getInt("previousTimeBreak"));
+    }
+
+    @Override
+    public void tick() {
+        if (!this.dead && ForgeEventFactory.getMobGriefingEvent(this.level, this)) {
+            boolean flag = false;
+
+            BlockPos blockpos = this.getOnPos().north();
+            BlockState blockstate = this.level.getBlockState(blockpos);
+            Block block = blockstate.getBlock();
+
+            BlockState blockstate2 = this.level.getBlockState(blockpos.above());
+            Block block2 = blockstate2.getBlock();
+
+
+            if (this.getCurrentTimeBreak() % 5 == 4) {
+               level.playLocalSound(blockpos.getX(), blockpos.getY(), blockpos.getZ(), blockstate.getSoundType().getHitSound(), SoundCategory.BLOCKS, 1F, 0.75F, false);
+            }
+
+            //set max destroy speed
+            int bp = (int) (blockstate.getDestroySpeed(this.level, blockpos) * 100);
+            this.setBreakingTime(bp);
+
+            //increase current
+            this.setCurrentTimeBreak(this.getCurrentTimeBreak() + (int) (1 * (this.getUseItem().getDestroySpeed(blockstate))));
+            float f = (float) this.getCurrentTimeBreak() / (float) this.getBreakingTime();
+
+            int i = (int) (f * 10);
+
+            if (i != this.getPreviousTimeBreak()) {
+                this.level.destroyBlockProgress(1, blockpos, i);
+                this.setPreviousTimeBreak(i);
+            }
+
+            if (this.getCurrentTimeBreak() >= this.getBreakingTime()) {
+                flag = this.level.destroyBlock(blockpos, true, this) || flag;
+                this.setCurrentTimeBreak(-1);
+                this.setBreakingTime(0);
+            }
+            super.tick();
+        }
+
+    }
 
 }
