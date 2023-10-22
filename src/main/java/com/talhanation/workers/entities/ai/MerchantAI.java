dd0d4e16692463ef79fda508a780c07fbd466532
talhanation <talhakantar@yahoo.com>
2023-10-22 23:35:10 +0200

    Update MerchantAI.java

--- src/main/java/com/talhanation/workers/entities/ai/MerchantAI.java
+++ src/main/java/com/talhanation/workers/entities/ai/MerchantAI.java
@@ -103,14 +103,14 @@ public class MerchantAI extends Goal {
             }
 
             case ARRIVED -> {
-                if(!arrivedMessage && merchant.getOwner() != null){
+                if(!arrivedMessage && merchant.getOwner() != null && !merchant.isCreative()){
                     merchant.tellPlayer(merchant.getOwner(), new TextComponent("I've arrived at my last waypoint."));
                     arrivedMessage = true;
                 }
                 if(merchant.isReturnTimeElapsed() || merchant.getReturning()){
                     merchant.setReturning(true);
                     merchant.setCurrentReturningTime(0);
-                    if(!returningMessage && merchant.getOwner() != null){
+                    if(!returningMessage && merchant.getOwner() != null && !merchant.isCreative(){
                         merchant.tellPlayer(merchant.getOwner(), new TextComponent("I'm returning now."));
                         returningMessage = true;
                     }
@@ -119,7 +119,7 @@ public class MerchantAI extends Goal {
             }
 
             case HOME -> {
-                if(!homeMessage && merchant.getOwner() != null){
+                if(!homeMessage && merchant.getOwner() != null && !merchant.isCreative()){
                     merchant.tellPlayer(merchant.getOwner(), new TextComponent("I've arrived where i've started."));
                     homeMessage = true;
                 }
