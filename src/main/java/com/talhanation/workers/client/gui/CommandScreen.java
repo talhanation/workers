7693fc15d7e98a1f02a5a8766b2456cdab2632bc
talhanation <talhakantar@yahoo.com>
2023-10-27 17:19:31 +0200

    fixed crash
    fix crash

--- src/main/java/com/talhanation/workers/client/gui/CommandScreen.java
+++ src/main/java/com/talhanation/workers/client/gui/CommandScreen.java
@@ -65,7 +65,7 @@ public class CommandScreen extends ScreenBase<CommandMenu> {
 
     private void setButtons(){
         this.clearWidgets();
-        this.name = worker_names.get(index);
+
         int zeroLeftPos = leftPos + 150;
         int zeroTopPos = topPos - 40;
 
@@ -86,8 +86,7 @@ public class CommandScreen extends ScreenBase<CommandMenu> {
         this.sleepPosButton.active = blockpos != null && getBlockState().isBed(player.level, blockpos, player);
         this.chestPosButton.active = blockpos != null && player.level.getBlockEntity(blockpos) instanceof Container;
 
-        this.name = worker_names.get(index);
-
+        if(!worker_names.isEmpty() && index < worker_names.size()) this.name = worker_names.get(index);
         //Buttons for special workers
         if(name.contains("Merchant")){
             this.startTravelButton = this.setStartTravel(zeroLeftPos, zeroTopPos);
